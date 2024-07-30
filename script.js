2b5b7f477376d80aebd1ab4ebc33a92094378d88
Magician774 <hodjicsuper22@gmail.com>
2024-07-30 11:04:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//for(let i = 20; i >= 10; i-- ){
+  //console.log(i)
+  
+//}
+//let i = 0
+//hile (i < 10) {
+  //console.log(i);
+  //i = i + 3
+//}
+//let i = 0
+//do {
+ // console.log("hello");
+  //i++
+//}while (i < 5) 
+//for (let i = 0; i < 10; i++) {
+  //if (i % 2 == 0) {
+ 
+  //console.log(i);
+  //}
+
+//}
+let sum = 0
+for (let i = 0; i <= 10; i++) {
+  sum = i + i
+   
+}
+console.log(sum);
